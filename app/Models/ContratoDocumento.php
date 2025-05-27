da1b5545c43d6c9bb6923af8bb513d549f9d5191
Mani-9 <manu201119@hotmail.com>
2025-05-26 20:55:53 -0600

    imagenes

--- app/Models/ContratoDocumento.php
+++ app/Models/ContratoDocumento.php
@@ -49,8 +49,8 @@ class ContratoDocumento extends Model implements HasMedia
 
     public function registerMediaConversions(Media $media = null): void
     {
-        $this->addMediaConversion('thumb')->fit('crop', 50, 50);
-        $this->addMediaConversion('preview')->fit('crop', 120, 120);
+        //$this->addMediaConversion('thumb')->fit('crop', 50, 50);
+        //$this->addMediaConversion('preview')->fit('crop', 120, 120);
     }
 
     public function getArchivoAttribute()
